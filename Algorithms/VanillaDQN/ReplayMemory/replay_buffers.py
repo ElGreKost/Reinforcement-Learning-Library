ea7120ea7a31c67a2e038d1f6a23965676dad040
Panagiotis Konstantinos Kakkavas <81427842+ElGreKost@users.noreply.github.com>
2023-12-17 20:16:39 +0200

    simple DQN finished

---
+++ Algorithms/VanillaDQN/ReplayMemory/replay_buffers.py
@@ -0,0 +1,22 @@
+from collections import deque
+import random
+
+
+class ReplayBuffer:
+    """Available functions are:
+    - push (s,a,r,s,term,trunc)
+    - sample(batch_size)
+    - len()"""
+
+    def __init__(self, capacity):
+        self.buffer = deque(maxlen=capacity)
+
+    def push(self, state, action, reward, next_state, term, trunc):
+        experience = (state, action, reward, next_state, float(term), float(trunc))
+        self.buffer.append(experience)
+
+    def sample(self, batch_size):
+        return random.sample(self.buffer, batch_size)
+
+    def __len__(self):
+        return len(self.buffer)
